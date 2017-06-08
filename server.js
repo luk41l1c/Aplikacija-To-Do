28aa3486355c923cad8e402b2fc22f2304865aae
Luka Ilic <lyksenzii@gmail.com>
2017-06-08 15:06:07 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ function odgovorServera(request, response) {
             break;
     }
 }
-var port = process.env.PORT || 8000;
-//var server = http.createServer(odgovorServera);
-server.listen(port);
+
+var server = http.createServer(odgovorServera);
+server.listen(8080);
 console.log("Server startovan na portu 8080");
