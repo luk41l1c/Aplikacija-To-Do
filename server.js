432ab2b7facf3c5d5dd741bcb1d0912645e6ba6e
Luka Ilic <lyksenzii@gmail.com>
2017-06-08 15:31:02 +0200

    Create server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ function odgovorServera(request, response) {
             break;
     }
 }
-
+var port = process.env.PORT || 9999;
 var server = http.createServer(odgovorServera);
-server.listen(8080);
+server.listen(port);
 console.log("Server startovan na portu 8080");
